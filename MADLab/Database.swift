b8ef5f5aa7ee804cea6826fd2787676d7af3b8b0
Rodolfo Martinez <rodolfo.martinez@mail.utoronto.ca>
2015-07-29 12:38:46 -0400

    added Database class

---
+++ MADLab/Database.swift
@@ -0,0 +1,64 @@
+//
+//  Database.swift
+//  MADLab
+//
+//  Created by Rodolfo Martinez on 2015-07-29.
+//  Copyright (c) 2015 madlab. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Database{
+    class func logIn(userName: String, password: String){
+        PFUser.logInWithUsernameInBackground(userName, password: password) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+                let alert = UIAlertView()
+                alert.title = "Success"
+                alert.message = "Welcome!"
+                alert.addButtonWithTitle("OK")
+                alert.show()
+            } else {
+                // The login failed. Check error to see why.
+                let errorString = error!.userInfo?["error"] as? String
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = errorString
+                alert.addButtonWithTitle("OK")
+                alert.show()
+            }
+        }
+    }
+    
+    class func signUp(username: String, email: String, password: String) {
+        var user = PFUser()
+        user.username = username
+        user.email = email
+        user.password = password
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo?["error"] as? String
+                // Show the errorString somewhere and let the user try again.
+                
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = errorString
+                alert.addButtonWithTitle("OK")
+                alert.show()
+                
+            } else {
+                // Hooray! Let them use the app now.
+                
+                let alert = UIAlertView()
+                alert.title = "Success"
+                alert.message = "Welcome!"
+                alert.addButtonWithTitle("OK")
+                alert.show()
+            }
+        }
+    }
+}
